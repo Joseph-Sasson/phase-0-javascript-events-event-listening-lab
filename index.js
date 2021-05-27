65a826b3b72de122075d363eff61ec82910cf0f1
Joseph <josephsasson1@gmail.com>
2021-05-27 12:04:45 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-function addingEventListener() {
+function addingEventListener(){
+  const input = document.getElementById('input')
+    input.addEventListener('click',
+      function () {
+        alert('I was clicked!')
+  })
 }
+addingEventListener()
+
+
+/*
+const input = document.getElementById('input');
+  input.addEventListener('click',
+    function() {
+    alert('I was clicked by you!');
+}); 
+*/
+
+/*
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert); 
+*/
